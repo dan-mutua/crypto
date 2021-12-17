5917fda560004c7f362e0b9095cb8d9d98de1d3e
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-12-18 01:20:27 +0300

    display data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
 import './App.css';
 import axios from 'axios';
 import React,{useState,useEffect}  from 'react';
+import Coin from './Coin';
 
 function App() {
+  const [coins,setCoins]=useState([]);
+  const[search, setSearch]=useState('')
 
 
   useEffect(() =>{
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=KSH&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-  })
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res =>{
+      setCoins(res.data);
+      
+    })
+  });
+
+  const handleChange = e =>{
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
-    <div className="App">
-     <p>Just trying this shit</p>
+    <div className="coin-app">
+     <div className="coin-search">
+       <h3 className="coin-text">Search currency</h3>
+       <form>
+         <input type="text" placeholder="Search" className="coin-input" onChange={handleChange}/>
+       </form>
+     </div>
+     {filteredCoins.map(coin =>{
+       return(
+         <Coin 
+         key={coin.id} 
+         name={coin.name} 
+         image={coin.image}
+         symbol={coin.symbol}
+         marketcap={coin.market_cap}
+         price={coin.current_price}
+         priceChange={coin.price_change_percentage_24h}
+         volume={coin.total_volume}
+         />
+       )
+     })}
     </div>
   );
 }
---
+++ src/Coin.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Coin({name,image,symbol,price,volume,priceChange,marketcap}) {
+    return (
+        <div className="coin-container">
+            <div className="coin-row">
+                <div className="coin">
+                    <img src={image} alt="crypto"/>
+                    <h3>{name}</h3>
+                    <p className="coin-symbol">{ symbol} </p>
+                </div>
+                <div className="coin-data">
+                    <p className="coin-price">$ {price} </p>
+                    <p className="coin-volume">${volume.toLocaleString()} </p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red ">{ priceChange}%</p> 
+                    ): (<p className="coin-percent blue ">{ priceChange}%</p>)}
+                    <p className="coin-marketcap">
+                        Mkt Cap: ${marketcap.toLocaleString()}
+                    </p>
+                </div>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Coin
