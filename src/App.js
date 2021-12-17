0ea7263394b61c25a1330af094bc4c393437cfcf
dan-mutua <daniel.mutua@student.moringaschool.com>
2021-12-17 23:29:17 +0300

    se up get link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
-import React  from 'react';
+import axios from 'axios';
+import React,{useState,useEffect}  from 'react';
 
 function App() {
+
+
+  useEffect(() =>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=KSH&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  })
+
   return (
     <div className="App">
      <p>Just trying this shit</p>
